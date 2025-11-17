33a2804f219ba6e67e7a50f838ce2e974cc307f6
sowndharya <v.sowndharya2000@gmail.com>
2025-11-17 20:40:10 +0530

    Message

--- header.php
+++ header.php
@@ -49,24 +49,27 @@
 	</div>
 
 	
+<nav class="navbar navbar-expand-lg navbar-dark" id="ftco-navbar">
+    <div class="container-fluid"> <!-- Use container-fluid to take full width -->
+        <a class="navbar-brand" href="index">Convention</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-		<div class="container">
-			<a class="navbar-brand" href="index">Convention</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="oi oi-menu"></span> Menu
-			</button>
-<div class="collapse navbar-collapse" id="ftco-nav">
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item active"><a href="index" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="about" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="retreats" class="nav-link">Retreats</a></li>
-    <li class="nav-item"> <a href="affirmation-technique" class="nav-link">Affirmation Technique</a></li>
-        <li class="nav-item"><a href="communities" class="nav-link">Communities</a></li>
-        <li class="nav-item"><a href="opportunities" class="nav-link">Opportunities</a></li>
-        <li class="nav-item"><a href="events" class="nav-link">Events</a></li>
-        <li class="nav-item"><a href="contact" class="nav-link">Contact</a></li>
-    </ul>
-</div>
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a class="nav-link" href="index">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="retreats">Retreats</a></li>
+                <li class="nav-item"><a class="nav-link" href="affirmation-technique">Affirmation Technique</a></li>
+                <li class="nav-item"><a class="nav-link" href="communities">Communities</a></li>
+                <li class="nav-item"><a class="nav-link" href="opportunities">Opportunities</a></li>
+                <li class="nav-item"><a class="nav-link" href="events">Events</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
+
+	
 <!-- END nav -->
