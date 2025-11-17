911ab801d4219b48a496a2a10cca7e6e0654d836
sowndharya <v.sowndharya2000@gmail.com>
2025-11-17 15:39:17 +0530

    message

--- Opportunities.php
+++ Opportunities.php
@@ -1,108 +1,264 @@
 <?php include 'header.php'; ?>
- 
-<section class="retreat-hero">
-    <div class="container">
-        <h1>Reset Your Mind. Realign Your Goals. Rebuild Your Business.</h1>
-        <p>Our retreats are deep, immersive programs designed for individuals who want to pause, reflect, and transform their inner and outer life.</p>
-    </div>
-</section>
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row no-gutters slider-text align-items-end">
+				<div class="col-md-9 ftco-animate pb-5">
+					<p class="breadcrumbs"><span class="mr-2"><a href="index">Home <i class="fa fa-chevron-right"></i></a></span> <span>Blog <i class="fa fa-chevron-right"></i></span></p>
+					<h1 class="mb-0 bread">Our Blog</h1>
+				</div>
+			</div>
+		</div>
+	</section>
 
- <!-- Retreat Benefits -->
-<section class="retreat-benefits">
-    <div class="container">
-        <div class="items">
-            <div class="items-head">
-                <p>Retreat Benefits</p>
-                <hr>
-            </div>
-            <div class="items-body">
-                <div class="items-body-content">
-                    <span>Release past emotional blocks</span>
-                    <i class="fa fa-angle-right"></i>
-                </div>
-                <div class="items-body-content">
-                    <span>Remove fear, overthinking & negativity</span>
-                    <i class="fa fa-angle-right"></i>
-                </div>
-                <div class="items-body-content">
-                    <span>Learn powerful affirmations</span>
-                    <i class="fa fa-angle-right"></i>
-                </div>
-                <div class="items-body-content">
-                    <span>Build clarity for business</span>
-                    <i class="fa fa-angle-right"></i>
-                </div>
-                <div class="items-body-content">
-                    <span>Gain momentum & motivation</span>
-                    <i class="fa fa-angle-right"></i>
-                </div>
-                <div class="items-body-content">
-                    <span>Reset your mindset to success</span>
-                    <i class="fa fa-angle-right"></i>
-                </div>
-                <div class="items-body-content">
-                    <span>Connect with like-minded people</span>
-                    <i class="fa fa-angle-right"></i>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-md-7">
+					<div class="row d-flex">
+						<div class="col-lg-6 ftco-animate">
+							<div class="blog-entry">
+								<a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');">
+								</a>
+								<div class="text d-block text-center">
+									<div class="meta">
+										<p>
+											<a href="#"><span class="fa fa-calendar mr-2"></span>Oct. 04, 2020</a>
+											<a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+											<a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+										</p>
+									</div>
+									<h3 class="heading"><a href="#">Applying your design principles</a></h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+								</div>
+							</div>
+						</div>
 
-<!-- Retreat Experience -->
-<section class="retreat-experience">
-    <div class="retreat-experience__wrapper">
-        <div class="retreat-experience__header">
-            <h2>Inside the Retreat</h2>
-        </div>
-        
-        <div class="retreat-experience__list">
-            <div class="retreat-experience__item">
-                <h5>Guided affirmations</h5>
-                <p>Experience deep inner calm with guided affirmation sessions designed to reset your mindset.</p>
-            </div>
-            <div class="retreat-experience__item">
-                <h5>Breakthrough mindset exercises</h5>
-                <p>Practical exercises to overcome limiting beliefs and foster mental clarity.</p>
-            </div>
-            <div class="retreat-experience__item">
-                <h5>Vision & goal setting</h5>
-                <p>Define your personal and business goals with structured planning sessions.</p>
-            </div>
-            <div class="retreat-experience__item">
-                <h5>Business direction planning</h5>
-                <p>Strategic guidance to align your business with your vision and priorities.</p>
-            </div>
-            <div class="retreat-experience__item">
-                <h5>Energy healing & grounding</h5>
-                <p>Reconnect with your energy and achieve balance through guided practices.</p>
-            </div>
-            <div class="retreat-experience__item">
-                <h5>Mentor-led coaching circles</h5>
-                <p>Learn and grow from peer discussions and mentor-led coaching sessions.</p>
-            </div>
-        </div>
-    </div>
-</section>
+						<div class="col-lg-6 ftco-animate">
+							<div class="blog-entry">
+								<a href="blog-single.html" class="block-20" style="background-image: url('images/image_2.jpg');">
+								</a>
+								<div class="text d-block text-center">
+									<div class="meta">
+										<p>
+											<a href="#"><span class="fa fa-calendar mr-2"></span>Oct. 04, 2020</a>
+											<a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+											<a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+										</p>
+									</div>
+									<h3 class="heading"><a href="#">Applying your design principles</a></h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 ftco-animate">
+							<div class="blog-entry">
+								<a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');">
+								</a>
+								<div class="text d-block text-center">
+									<div class="meta">
+										<p>
+											<a href="#"><span class="fa fa-calendar mr-2"></span>Oct. 04, 2020</a>
+											<a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+											<a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+										</p>
+									</div>
+									<h3 class="heading"><a href="#">Applying your design principles</a></h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 ftco-animate">
+							<div class="blog-entry">
+								<a href="blog-single.html" class="block-20" style="background-image: url('images/image_4.jpg');">
+								</a>
+								<div class="text d-block text-center">
+									<div class="meta">
+										<p>
+											<a href="#"><span class="fa fa-calendar mr-2"></span>Oct. 04, 2020</a>
+											<a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+											<a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+										</p>
+									</div>
+									<h3 class="heading"><a href="#">Applying your design principles</a></h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 ftco-animate">
+							<div class="blog-entry">
+								<a href="blog-single.html" class="block-20" style="background-image: url('images/image_5.jpg');">
+								</a>
+								<div class="text d-block text-center">
+									<div class="meta">
+										<p>
+											<a href="#"><span class="fa fa-calendar mr-2"></span>Oct. 04, 2020</a>
+											<a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+											<a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+										</p>
+									</div>
+									<h3 class="heading"><a href="#">Applying your design principles</a></h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 ftco-animate">
+							<div class="blog-entry">
+								<a href="blog-single.html" class="block-20" style="background-image: url('images/image_6.jpg');">
+								</a>
+								<div class="text d-block text-center">
+									<div class="meta">
+										<p>
+											<a href="#"><span class="fa fa-calendar mr-2"></span>Oct. 04, 2020</a>
+											<a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+											<a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+										</p>
+									</div>
+									<h3 class="heading"><a href="#">Applying your design principles</a></h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row mt-5">
+						<div class="col">
+							<div class="block-27">
+								<ul>
+									<li><a href="#">&lt;</a></li>
+									<li class="active"><span>1</span></li>
+									<li><a href="#">2</a></li>
+									<li><a href="#">3</a></li>
+									<li><a href="#">4</a></li>
+									<li><a href="#">5</a></li>
+									<li><a href="#">&gt;</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
 
-<section class="retreat-types">
-  <div class="container">
-    <h2>Types of Retreats</h2>
-    <div class="retreat-types__cards">
-      <div class="retreat-card">
-        <h3>Business Breakthrough Retreat</h3>
-      </div>
-      <div class="retreat-card">
-        <h3>Entrepreneur Confidence Reset Camp</h3>
-      </div>
-      <div class="retreat-card">
-        <h3>21-Day Inner Transformation Journey</h3>
-      </div>
-    </div>
-  </div>
-</section>
 
+				<div class="col-lg-4 col-md-5 sidebar ftco-animate pl-md-5 mt-5 mt-md-0 pt-md-0 pt-5">
+					<div class="sidebar-box bg-light rounded">
+						<form action="#" class="search-form">
+							<div class="form-group">
+								<span class="icon fa fa-search"></span>
+								<input type="text" class="form-control" placeholder="Search...">
+							</div>
+						</form>
+					</div>
 
+					<div class="sidebar-box ftco-animate">
+						<h3>Recent Blog</h3>
+						<div class="block-21 mb-4 d-flex justify-content-between">
+							<a class="blog-img" style="background-image: url(images/image_1.jpg);"></a>
+							<div class="text pl-3">
+								<h3 class="heading"><a href="#">Marketing Strategies for Digital Ecosystem</a></h3>
+								<div class="meta">
+									<div><a href="#"><span class="fa fa-calendar"></span> Oct. 04, 2020</a></div>
+									<div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
+								</div>
+							</div>
+						</div>
+						<div class="block-21 mb-4 d-flex justify-content-between">
+							<a class="blog-img" style="background-image: url(images/image_2.jpg);"></a>
+							<div class="text pl-3">
+								<h3 class="heading"><a href="#">Marketing Strategies for Digital Ecosystem</a></h3>
+								<div class="meta">
+									<div><a href="#"><span class="fa fa-calendar"></span> Oct. 04, 2020</a></div>
+									<div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
+								</div>
+							</div>
+						</div>
+						<div class="block-21 mb-4 d-flex justify-content-between">
+							<a class="blog-img" style="background-image: url(images/image_3.jpg);"></a>
+							<div class="text pl-3">
+								<h3 class="heading"><a href="#">Marketing Strategies for Digital Ecosystem</a></h3>
+								<div class="meta">
+									<div><a href="#"><span class="fa fa-calendar"></span> Oct. 04, 2020</a></div>
+									<div><a href="#"><span class="fa fa-comment"></span> 19</a></div>
+								</div>
+							</div>
+						</div>
+					</div>
 
- 
- <?php include 'footer.php'; ?>
+					<div class="sidebar-box ftco-animate">
+						<h3>Tag Cloud</h3>
+						<div class="tagcloud">
+							<a href="#" class="tag-cloud-link">speak</a>
+							<a href="#" class="tag-cloud-link">conference</a>
+							<a href="#" class="tag-cloud-link">meeting</a>
+							<a href="#" class="tag-cloud-link">seminar</a>
+							<a href="#" class="tag-cloud-link">chat</a>
+							<a href="#" class="tag-cloud-link">talk</a>
+							<a href="#" class="tag-cloud-link">web</a>
+							<a href="#" class="tag-cloud-link">design</a>
+						</div>
+					</div>
+
+					<div class="sidebar-box ftco-animate">
+						<h3>Paragraph</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam vero culpa sapiente consectetur similique, inventore eos fugit cupiditate numquam!</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="ftco-intro img" style="background-image: url(images/bg_3.jpg);">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-12 heading-section heading-section-white text-center ftco-animate">
+					<h2 class="mb-4">Book Your Tickets Now</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12">
+					<form action="#" class="registry-form ftco-animate">
+						<div class="row">
+							<div class="col-md">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="First Name">
+								</div>
+							</div>
+							<div class="col-md">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Last Name">
+								</div>
+							</div>
+							<div class="col-md">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Phone">
+								</div>
+							</div>
+							<div class="col-md">
+								<div class="form-group">
+									<div class="form-field">
+										<div class="select-wrap">
+											<div class="icon"><span class="fa fa-chevron-down"></span></div>
+											<select name="" id="" class="form-control">
+												<option value="">Select Your Price</option>
+												<option value="">$10</option>
+												<option value="">$20</option>
+												<option value="">$25</option>
+												<option value="">$30</option>
+												<option value="">$50</option>
+												<option value="">$75</option>
+												<option value="">$100</option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md">
+								<div class="form-group">
+									<input type="submit" value="Register Now" class="btn btn-primary py-3 px-4">
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+       <?php include 'footer.php'; ?>
